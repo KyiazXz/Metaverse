103f4ea7f8e6d9a0fc6fd16dfbc3d01cd1f45595
KYIAZBEK ZHOLDOSHEV <111270349+KyiazXz@users.noreply.github.com>
2023-03-29 18:19:20 +0300

    commit

---
+++ app/head.js
@@ -0,0 +1,10 @@
+const Head = () => (
+  <>
+    <title>Metaversus</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="metaverse" />
+    <link rel="icon" href="/favicon.ico" />
+  </>
+);
+
+export default Head;
